f6739966375a0c3c665ef487711fdc336c2a381c
mug1wara26 <aloysiusgsq@gmail.com>
2020-10-09 20:22:43 +0800

    fixed settings bug

--- src/Controllers/SettingsController.java
+++ src/Controllers/SettingsController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
